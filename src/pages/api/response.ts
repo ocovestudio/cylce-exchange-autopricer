f50f09392a9a31f5c350fb8fb6cfc937e1bec6d1
billy <billymylesberkouwer@gmail.com>
2023-02-28 16:18:02 +0000

    fetch-from-api

---
+++ src/pages/api/response.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Airtable from 'airtable'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  models: string[],
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: `${process.env.API_KEY}`
+  });
+
+  var base = Airtable.base(`${process.env.DATABASE_ID}`);
+
+  return new Promise((resolve, reject) => {
+    base('99 Data').select({
+      view: 'Pricing Model'
+    }).firstPage(function (err, records) {
+      if (err) {
+        res.status(401).send(err);
+        reject(err);
+      }
+      if (records) {
+        const retreivedRecords = records.map(function (record) {
+          return ('Model: ' + record.get('Model'));
+        });
+        res.status(200).json({ models: retreivedRecords });
+        resolve({ bikes: retreivedRecords });
+        res.end();
+      }
+    });
+  })
+}
