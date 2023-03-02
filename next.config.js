beacde2db0a928ebc076b7496d2b1f69d19e3236
ocovestudio <office@ocove.studio>
2023-03-02 15:09:24 +0000

    next-env-variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    AIRTABLE_KEY: process.env.AIRTABLE_KEY,
+    DATABASE_ID: process.env.DATABASE_ID
+  }
 }
 
 module.exports = nextConfig
